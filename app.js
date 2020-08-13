d41131c64a573d129bb628cc0b526977017f7c67
Kavitha Suresh Kumar <kavisuresh@in.ibm.com>
2020-08-14 00:03:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,4 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, function(){
-  console.log('Server is running on port 3000')
-});
+app.listen(PORT, () => console.log(`Server is running on port $(PORT)`);
